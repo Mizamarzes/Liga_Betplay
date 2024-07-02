bf5ec0c50929277aab03f823cace191b77a41eaf
mizamarzes <mizamarzes@gmail.com>
2024-07-01 20:27:39 -0500

    v1.1.2 - Database Improved and added necesary tables

--- src/main/java/com/liga_betplay/controller/TeamController.java
+++ src/main/java/com/liga_betplay/controller/TeamController.java
@@ -1,9 +1,5 @@
 package com.liga_betplay.controller;
 
-import java.util.Hashtable;
-
-import com.liga_betplay.model.Team;
-
 public class TeamController {
-    public Hashtable <Integer, Team> teams = new Hashtable<>();
+    
 }
